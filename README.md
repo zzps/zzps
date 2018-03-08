12d0cb63eee18eede6648c4cfd8f7ee7cdbe0075
ZZPS\Administrator <1421697447@qq.com>
2018-03-08 17:14:16 +0800

    no message

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # zzps
 simple and high performance web framework for go
+---by author jianglinhui
--- router.go
+++ router.go
@@ -62,7 +62,6 @@ func (router *zRouter)ServeHTTP(rw http.ResponseWriter, r *http.Request)  {
 	context.Reset(rw,r)
 	defer router.contextPool.Put(context)
 	urlPath := r.URL.Path
-
 	if router.enableStaticSupport {
 		if strings.HasPrefix(urlPath,router.staticUrlPrefix){
 			filePath := router.staticDir + strings.TrimPrefix(urlPath,router.staticUrlPrefix)
